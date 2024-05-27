92ea3017d08b81ac695d77f1ce81305b4e82b2e1
BrunoRammon <BrunoRammon>
2024-05-27 20:32:36 -0300

    add week 2 homework

--- 02_exp_tracking/homework/register_model.py
+++ 02_exp_tracking/homework/register_model.py
@@ -63,7 +63,7 @@ def run_register_model(data_path: str, top_n: int):
         experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=top_n,
-        order_by=["metrics.rmse ASC"]
+        order_by=["metrics.test_rmse ASC"]
     )
     for run in runs:
         train_and_log_model(data_path=data_path, params=run.data.params)
@@ -71,9 +71,17 @@ def run_register_model(data_path: str, top_n: int):
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
     # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.rmse ASC"]
+    )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    model_uri = f"runs:/{best_run.info.run_id}/model"
+    mlflow.register_model(model_uri,
+                          "nycTaxiTripDurationPredictionRandomForestRegressionModel")
 
 
 if __name__ == '__main__':
